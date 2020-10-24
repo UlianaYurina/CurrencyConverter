69e35a2cf41b46e8ec5ada0e790c5e881eec6592
kotol@icloud.com <kotol@icloud.com>
2020-10-24 13:53:48 +0300

    added cbr service method

---
+++ src/main/java/com/currencyconverter/controller/AppController.java
@@ -0,0 +1,21 @@
+package com.currencyconverter.controller;
+
+import com.currencyconverter.dto.TransferDto;
+import com.currencyconverter.dto.TransferResult;
+import com.currencyconverter.service.ConverterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @Autowired
+    private ConverterService converterService;
+
+    @PostMapping("/exchange")
+    public void getExchange (@RequestBody TransferDto transferDto) {
+        converterService.getCourses();
+    }
+}
